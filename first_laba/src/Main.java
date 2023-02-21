6e57a8c0f6c3dd9f3e62af2b98ab6a3cb625fe6f
thomas.brnf <57502071+thomasbrnf@users.noreply.github.com>
2023-02-21 20:19:59 +0200

    Add files via upload

--- first_laba/src/Main.java
+++ first_laba/src/Main.java
@@ -2,26 +2,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter n: ");
-        int n = scanner.nextInt();
+        short n = scanner.nextShort();
         System.out.print("Enter m: ");
-        int m = scanner.nextInt();
-        System.out.println(fun(n,m));
-    }
-    private static float fun(int a, int b) {
-        final int C = 2;
-        float ans = 0, calc = 0;
-        float i = 0, j = 0;
-        for (int n = 0; n <= a; n++){
-            ans += calc;
-            for (int m = 0; m <= b; m++){
-                calc += ((i-j)/(i+C));
-                ++j;
-            }
-            ++i;
+        short m = scanner.nextShort();
+        try {
+            System.out.println(Formula.fun(n,m));
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
         }
-        return ans;
     }
 }
 
