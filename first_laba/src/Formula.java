89595729e72ce77cf47b9aed8a4f263f7998302c
thomas.brnf <57502071+thomasbrnf@users.noreply.github.com>
2023-02-21 23:23:08 +0200

    Add files via upload

--- first_laba/src/Formula.java
+++ first_laba/src/Formula.java
@@ -2,13 +2,11 @@ public class Formula {
     public static float fun(short n, short m) {
         final short C = 2;
         float ans = 0;
-        // summation of summation
+        // summation expression
         for (short i = 0; i <= n; i++){
-            // summation expression
             for (short j = 0; j <= m; j++){
                 ans += ((i-j)/(i+C));
             }
-            ans += ans;
         }
         return ans;
     }
